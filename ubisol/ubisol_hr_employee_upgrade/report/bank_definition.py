41e0aaf3b783399e7c8a7b5fbc29a664c5eaa129
bataa0718 <34029979+bataa0718@users.noreply.github.com>
2020-09-16 19:49:19 +0800

    no message

--- ubisol/ubisol_hr_employee_upgrade/report/bank_definition.py
+++ ubisol/ubisol_hr_employee_upgrade/report/bank_definition.py
@@ -1,17 +1,21 @@
 from odoo import api, models, _
+from odoo.exceptions import UserError
 
 class ReportDefinitionPdf(models.AbstractModel):
-    _name = 'report.ubisol_hr_employee_upgrade.report_definition'
+    _name = 'report.ubisol_hr_employee_upgrade.report_definition_pdf'
     _description = 'Bank Definition Report'
 
     @api.model
     def _get_report_values(self, docids, data=None):
         # import pdb
         # pdb.set_trace()
+        if not data.get('form'):
+            raise UserError(_("Form content is missing, this report cannot be printed."))
+        
         docs = self.env['create.bank_definition'].browse(data['form']['id'])    
         employee = self.env['hr.employee'].browse(data['form']['employee_id'][0])
         return {
             'doc_model': 'hr.employee',
-            'docs': docs,
-            'employee': employee
+            'organization_name': str(data['form']['organization_name']).upper(),
+            'employee': employee,
         }
