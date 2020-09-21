61bf9e5d930d0e16a6f16ae547ec920742e08ba7
bataa0718 <34029979+bataa0718@users.noreply.github.com>
2020-09-21 18:42:25 +0800

    timeline

--- ubisol/ubisol_hr_employee_shift/models/hr_employee_shift.py
+++ ubisol/ubisol_hr_employee_shift/models/hr_employee_shift.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 class HrEmployeeShift(models.Model):
     """Ээлж хуваарилалт"""
@@ -18,29 +20,62 @@ class HrEmployeeShift(models.Model):
     date_from = fields.Date(string='Starting Date')
     date_to = fields.Date(string='End Date')
     resource_calendar_ids = fields.Many2one('resource.calendar', 'Working Hours')
-
+    
     @api.model
     def create(self, vals):
-        # {'assign_type': 'department', 'name': 'schedule 1', 'date_from': '2020-09-01', 'date_to': '2020-09-30', 
-        # 'hr_department': 1, 'hr_employee': False, 'resource_calendar_ids': 6}
         shift = super(HrEmployeeShift, self).create(vals)
-        return shift
+        shift_template = self.env['resource.calendar'].browse(vals.get('resource_calendar_ids'))
 
-    def write(self, vals):
-        # employee = super(HrEmployee, self).write(vals)
-        # contract_values = []
-        # if self.contract_signed_date and self.create_contract:           
-        #     contract_values.append({
-        #         'name': self.name,
-        #         'employee_id': self.id,
-        #         'date_start': self.contract_signed_date,
-        #         'department_id': self.department_id.id,
-        #         'job_id': self.job_id.id,
-        #         'wage': 0
-        #     })
-        #     self.env['hr.contract'].create(contract_values)
-        # return employee
-        shift = super(HrEmployeeShift, self).create(vals)
+        if shift_template.shift_type == 'days':
+            day_ids = shift_template.normal_day_ids
+        else:
+            day_ids = shift_template.factory_day_ids
+            total_len = len(day_ids)
+            counter = 0
+
+        if vals.get('hr_department') == False:
+            employees = self.env['hr.employee'].search([('id', '=', vals.get('hr_employee'))])
+        else:
+            employees = self.env['hr.employee'].search([('department_id', '=', vals.get('hr_department'))])
+
+        fmt = '%Y-%m-%d'
+        date_from = datetime.strptime(vals.get('date_from'), fmt)
+        date_to = datetime.strptime(vals.get('date_to'), fmt)
+        dates_btwn = date_from
+
+        while dates_btwn <= date_to:
+            if shift_template.shift_type == 'days':
+                pass
+            else:
+                inside_counter = 0
+                for day in day_ids:
+                    if inside_counter == counter:
+                        for employee in employees:
+                            schedule_dict = {}
+                            schedule_dict['hr_department'] = vals.get('hr_department')
+                            schedule_dict['hr_employee'] = employee.id
+                            schedule_dict['date_from'] = vals.get('date_from')
+                            schedule_dict['date_to'] = vals.get('date_to')
+                            schedule_dict['work_day'] = dates_btwn.date()
+                            schedule_dict['hr_employee_shift'] = shift.id
+                            schedule_dict['hr_employee_shift_template'] = shift_template.id
+                            schedule_dict['hr_employee_shift_dayplan'] = day_ids[counter].id
+                            schedule_dict['shift_type'] = shift_template.shift_type
+                            # schedule_dict['week_day'] = day.week_day
+                            schedule_dict['day_period'] = day_ids[counter].day_period.id
+                            schedule_dict['lunch_time_from'] = day_ids[counter].lunch_time_from
+                            schedule_dict['lunch_time_to'] = day_ids[counter].lunch_time_to
+                            schedule_dict['start_work'] = day_ids[counter].start_work
+                            schedule_dict['end_work'] = day_ids[counter].end_work
+                            schedule = self.env['hr.employee.schedule'].create(schedule_dict)
+                            print(schedule)
+                            counter = counter + 1
+                    else:
+                        inside_counter = inside_counter + 1
+            if counter > total_len:
+                counter = 0
+            dates_btwn = dates_btwn + relativedelta(days=1) 
+
         return shift
 
 class HrEmployeeSchedule(models.Model):
@@ -53,10 +88,13 @@ class HrEmployeeSchedule(models.Model):
     hr_employee_shift = fields.Many2one('hr.employee.shift', string="Employee Shift", help="Employee Shift")
     hr_employee_shift_template = fields.Many2one('resource.calendar', 'Employee Shift Template')
     hr_employee_shift_dayplan = fields.Many2one('resource.calendar.shift', 'Employee Shift Plan of Day')
+    date_from = fields.Date(string='Starting Date')
+    date_to = fields.Date(string='End Date')
+    work_day = fields.Date(string='End Date')
     shift_type = fields.Selection([
         ('days', 'Days'),
         ('shift', 'Shift')
-    ], default="days", tracking=True)
+    ], default="shift", tracking=True)
     week_day = fields.Selection([
         ('0', 'Monday'),
         ('1', 'Tuesday'),
