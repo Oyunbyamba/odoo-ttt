90b888dd81a7780f873441eea88647131850678c
Sainbayar <sainbayarus@gmail.com>
2020-10-12 10:54:52 +0800

    no message

--- ubisol/ubisol_biometric_device/models/__init__.py
+++ ubisol/ubisol_biometric_device/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import models, zk_device
+from . import models, biometric_device, biometric_attendance
---
+++ ubisol/ubisol_biometric_device/models/biometric_attendance.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import pytz
+import sys
+import datetime
+import logging
+import binascii
+
+from odoo import models, fields, api, exceptions, _
+
+
+class BiometricAttendance(models.Model):
+
+    _name = 'biometric.attendance'
+
+    pin_code = fields.Char(string='Pin Code')
+    punch_date_time = fields.Datetime('DateTime ', required=False)
+    device_id = fields.Many2one('biometric.machine', string='Төхөөрөмжийн нэр')
+    attendance_data = fields.Char(string='Attendance data')
+    attendance_data1 = fields.Char(string='Attendance data1')
+    attendance_data2 = fields.Char(string='Attendance data2')
+    attendance_data3 = fields.Char(string='Attendance data3')
