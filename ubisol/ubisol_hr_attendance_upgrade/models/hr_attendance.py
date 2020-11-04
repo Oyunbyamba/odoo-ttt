459867390a2dd684e6e3c3a87ec9f22e72488ac5
Ganjidmaa <ganjidmaa@ubisol.mn>
2020-11-04 15:33:16 +0800

    no message

--- ubisol/ubisol_hr_attendance_upgrade/models/hr_attendance.py
+++ ubisol/ubisol_hr_attendance_upgrade/models/hr_attendance.py
@@ -3,6 +3,19 @@ from odoo import fields, models, api
 class HrAttendance(models.Model):
     _inherit = "hr.attendance"
 
+    def _default_employee(self):
+        return self.env.user.employee_id
+
+    def _employee_id_domain(self):
+        if self.user_has_groups('hr_attendance.group_hr_attendance_user') or self.user_has_groups('hr_attendance.group_hr_attendance_manager'):
+            return []
+        if self.user_has_groups('ubisol_hr_attendance_upgrade.group_hr_attendance_responsible'):
+            return ['|', ('parent_id', '=', self.env.user.employee_id.id), ('user_id', '=', self.env.user.id)]
+        return [('user_id', '=', self.env.user.id)]
+
+    employee_id = fields.Many2one('hr.employee', string="Employee",
+                                  default=_default_employee, domain=_employee_id_domain,
+                                  required=True, ondelete='cascade', index=True)
     pin = fields.Char(related='employee_id.pin', string="PIN")
     fullname = fields.Char(compute="_compute_fullname", compute_sudo=True)
     department_id = fields.Many2one('hr.department', related='employee_id.department_id', string="Хэлтэс", store=True)
