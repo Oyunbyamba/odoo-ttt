ccd818b5ee444c10579d638e6d3a8ebf50693a0c
Ganjidmaa <ganjidmaa@ubisol.mn>
2020-11-06 19:59:37 +0800

    no message

--- ubisol/ubisol_hr_holidays/models/hr_leave_request_type.py
+++ ubisol/ubisol_hr_holidays/models/hr_leave_request_type.py
@@ -9,6 +9,13 @@ class UbisolHolidaysType(models.Model):
     _inherit = 'hr.leave.type' 
     _description = "Leave type"
    
-    vacation = fields.Boolean('Ээлжийн амралт', default=False)
+    request_status_type = fields.Selection([
+        ('paid', 'Цалинтай'),
+        ('unpaid', 'Цалингүй'),
+        ('vacation', 'Ээлжийн амралт'),
+        ('overtime', 'Илүү цаг'),
+        ('attendance', 'Ирц нөхөлт'),
+        ('outside_work', 'Гадуур ажил')
+        ], string='Чөлөөний төрөл', required=True)
     one_step_days = fields.Integer('1 шатлалт чөлөөнд тооцох хоног', default=3)
 
