57382d51343c3cb199e309001532a8885973c7c0
Ganjidmaa <ganjidmaa@ubisol.mn>
2021-02-23 20:10:30 +0800

    total xlx report header update

--- ubisol/ubisol_hr_attendance_report/models/attendance_report_interval.py
+++ ubisol/ubisol_hr_attendance_report/models/attendance_report_interval.py
@@ -7,6 +7,7 @@ import time
 import json
 import io
 import math
+import logging
 from odoo.exceptions import ValidationError
 from odoo.tools import date_utils
 try:
@@ -14,6 +15,7 @@ try:
 except ImportError:
     import xlsxwriter
 
+_logger = logging.getLogger(__name__)
 
 class AttendanceReport(models.TransientModel):
     _name = 'attendance.report.interval'
@@ -92,9 +94,13 @@ class AttendanceReport(models.TransientModel):
         sheet = workbook.add_worksheet()
 
         sheet.set_row(0, 50)
-        sheet.set_row(1, 30)
+        sheet.set_row(1, 15)
+        sheet.set_row(2, 15)
+        sheet.set_row(3, 15)
         sheet.set_column(0, 0, 20)
-        sheet.set_column(1, 12, 12)
+        sheet.set_column(1, 12, 10)
+        sheet.set_column(13, 26, 5)
+        sheet.set_column(27, 27, 10)
 
         header_format = workbook.add_format({
             'bold': 1,
@@ -114,46 +120,102 @@ class AttendanceReport(models.TransientModel):
             'valign': 'vcenter',
             'text_wrap': True
         })
+        header_rotation_format = workbook.add_format({
+            'bold': 1,
+            'border': 1,
+            'align': 'center',
+            'valign': 'vcenter',
+            'text_wrap': True,
+            'rotation': '90'
+        })
+        footer_format = workbook.add_format({
+            'bold': 0,
+            'border': 0,
+            'align': 'initial',
+            'valign': 'vcenter',
+            'text_wrap': False
+        })
+        footer_format_bold = workbook.add_format({
+            'bold': 1,
+            'border': 0,
+            'align': 'initial',
+            'valign': 'vcenter',
+            'text_wrap': False
+        })
 
         headers = data['header']
         lines = data['data']
         filters = data['filters']
 
-        title = filters['start_date'] + '-аас ' + \
-            filters['end_date'] + '-ны хоорондох ирцийн мэдээлэл'
-
-        sheet.merge_range('A1:M1', title, merge_format)
-
-        row = 1
+        row = 4
 
         # write data (for column title)
-        for i, h in enumerate(headers):
-            sheet.write(row, i, h[1], header_format)
+        sheet.merge_range('A2:A4', 'Овог Нэр', merge_format)
+        sheet.merge_range('B2:B4', 'Регистрийн дугаар', merge_format)
+        sheet.merge_range('C2:D3', 'Ажиллавал зохих', merge_format)
+        sheet.merge_range('E2:F3', 'Ажилласан', merge_format)
+        sheet.merge_range('G2:M2', 'Илүү цаг', merge_format)
+        sheet.merge_range('G3:H3', 'Цалин бодогдох илүү цаг', merge_format)
+        sheet.merge_range('I3:I4', 'Батлагдсан илүү цаг', merge_format)
+        sheet.merge_range('J3:J4', 'Нийт илүү цаг', merge_format)
+        sheet.merge_range('K3:M3', 'Үүнээс', merge_format)
+        sheet.merge_range('N2:AA2', 'Ажиллаагүй', merge_format)
+        sheet.merge_range('N3:O3', 'БҮГД', merge_format)
+        sheet.merge_range('P3:Q3', 'Чөлөөтэй /цалинтай/', merge_format)
+        sheet.merge_range('R3:S3', 'Чөлөөтэй /цалингүй/', merge_format)
+        sheet.merge_range('T3:U3', 'Өвчтэй', merge_format)
+        sheet.merge_range('V3:W3', 'Ээлжийн амралттай', merge_format)
+        sheet.merge_range('X3:Y3', 'Жирэмсэний амралттай', merge_format)
+        sheet.merge_range('Z3:AA3', 'Тасалсан', merge_format)
+        sheet.merge_range('AB2:AB4', 'Хоцорсон цаг', merge_format)
+        sheet.write(3, 2, 'Өдөр', header_format)
+        sheet.write(3, 3, 'Нийт цаг', header_format)
+        sheet.write(3, 4, 'Өдөр', header_format)
+        sheet.write(3, 5, 'Нийт цаг', header_format)
+        sheet.write(3, 6, 'Илүү цаг', header_format)
+        sheet.write(3, 7, 'Баяр ёслолын өдөр ажилласан илүү цаг', header_format)
+        sheet.write(3, 10, 'Хуруу дарж авах илүү цаг', header_format)
+        sheet.write(3, 11, 'Баяр ёслолын өдөр ажилласан илүү цаг', header_format)
+        sheet.write(3, 12, 'Хүсэлтээр баталгаажсан илүү цаг', header_format)
+        sheet.write(3, 13, 'Өдөр', header_rotation_format)
+        sheet.write(3, 14, 'Цаг', header_rotation_format)
+        sheet.write(3, 15, 'Өдөр', header_rotation_format)
+        sheet.write(3, 16, 'Цаг', header_rotation_format)
+        sheet.write(3, 17, 'Өдөр', header_rotation_format)
+        sheet.write(3, 18, 'Цаг', header_rotation_format)
+        sheet.write(3, 19, 'Өдөр', header_rotation_format)
+        sheet.write(3, 20, 'Цаг', header_rotation_format)
+        sheet.write(3, 21, 'Өдөр', header_rotation_format)
+        sheet.write(3, 22, 'Цаг', header_rotation_format)
+        sheet.write(3, 23, 'Өдөр', header_rotation_format)
+        sheet.write(3, 24, 'Цаг', header_rotation_format)
+        sheet.write(3, 25, 'Өдөр', header_rotation_format)
+        sheet.write(3, 26, 'Цаг', header_rotation_format)
+
+        # write column index
+        i = 0
+        while i < 28:
+            sheet.write(4, i, i, header_format)
+            i += 1
+
+        # last row
         row += 1
 
-        # Set data
-        for l in lines:
-            for i, h in enumerate(headers):
-                index = h[0]
-                try:
-                    if l[index]:
-                        if index == 'hr_employee_shift':
-                            sheet.write(row, i, l[index][1], border_format)
-                        elif index == 'worked_days' or index == 'take_off_day' or index == 'full_name' or index == 'work_day' or index == 'work_days':
-                            sheet.write(row, i, l[index], border_format)
-                        else:
-                            sheet.write(row, i, self._set_hour_format(
-                                l[index]), border_format)
-                    else:
-                        if index == 'worked_days' or index == 'take_off_day':
-                            sheet.write(row, i, 0, border_format)
-                        elif index == 'check_in' or index == 'check_out':
-                            sheet.write(row, i, '-', border_format)
-                        else:
-                            sheet.write(row, i, '00:00', border_format)
-                except KeyError:
-                    sheet.write(row, i, '', border_format)
-            row += 1
+        sheet.write(row, 1, 'Тушаалаар баталгаажиж олговол зохих илүү цагийн хязгаар: ', footer_format)
+        sheet.write(row, 12, 'Цагийн дээд хязгаартай байх /10-аас ихгүй гм/: ', footer_format)
+        row += 2
+        sheet.write(row, 1, 'Хянасан: ', footer_format_bold)
+        sheet.write(row, 10, 'Шалгасан: ', footer_format_bold)
+        row += 1
+        sheet.write(row, 1, 'Төлөвлөлт, хөгжүүлэлтийн алба ', footer_format)
+        sheet.write(row, 10, 'Нягтлан бодогч: ', footer_format)
+        row += 2
+        sheet.write(row, 2, 'Дарга: ', footer_format)
+        row += 2
+        sheet.write(row, 1, 'Нийгмийн асуудал, хүний нөөцийн алба ', footer_format)
+        row += 2
+        sheet.write(row, 2, 'Ахлах мэргэжилтэн: ', footer_format)
+
 
         workbook.close()
         output.seek(0)
