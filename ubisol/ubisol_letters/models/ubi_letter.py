ba7a5dc053114385cc190021409b721b6346b744
tsenguun-ubisol <80658114+tsenguun-ubisol@users.noreply.github.com>
2021-03-29 13:25:53 +0800

    letter ubisol

--- ubisol/ubisol_letters/models/ubi_letter.py
+++ ubisol/ubisol_letters/models/ubi_letter.py
@@ -29,40 +29,27 @@ class UbiLetter(models.Model):
     is_local = fields.Boolean(string='Дотоод бичиг эсэх', groups="hr.group_hr_user")
     is_received_letter = fields.Boolean(string='Ирсэн бичиг эсэх', groups="hr.group_hr_user")
     card_number = fields.Char(string='Картын дугаар', help="Картын дугаар", groups="hr.group_hr_user")
-    letter_number = fields.Char(
-        string='Бичгийн дугаар', help="Бичгийн дугаар", groups="hr.group_hr_user")
-    register_number = fields.Char(
-        string='Бүртгэлийн дугаар', help="Бүртгэлийн дугаар", groups="hr.group_hr_user")
-    letter_total_num = fields.Integer(
-        string='Хуудасны тоо', help="Хуудасны тоо", groups="hr.group_hr_user")
-    received_datetime = fields.Datetime(
-        string='Хүлээн авсан огноо', groups="hr.group_hr_user")
-    registered_datetime = fields.Datetime(
-        string='Бүртгэсэн огноо', groups="hr.group_hr_user")
-    return_datetime = fields.Datetime(
-        string='Хариу өгөх огноо', groups="hr.group_hr_user")
-    sent_datetime = fields.Datetime(
-        string='Илгээх огноо', groups="hr.group_hr_user")
+    letter_number = fields.Char(string='Бичгийн дугаар', help="Бичгийн дугаар", groups="hr.group_hr_user")
+    register_number = fields.Char(string='Бүртгэлийн дугаар', help="Бүртгэлийн дугаар", groups="hr.group_hr_user")
+    letter_total_num = fields.Integer(string='Хуудасны тоо', help="Хуудасны тоо", groups="hr.group_hr_user")
+    
+    received_datetime = fields.Datetime(string='Хүлээн авсан огноо', groups="hr.group_hr_user")
+    registered_datetime = fields.Datetime(string='Бүртгэсэн огноо', groups="hr.group_hr_user")
+    return_datetime = fields.Datetime(string='Хариу өгөх огноо', groups="hr.group_hr_user")
+   
+    sent_datetime = fields.Datetime(string='Илгээх огноо', groups="hr.group_hr_user")
 
-    partner_id = fields.Many2one(
-        'res.partner', string='Хэнээс', groups="hr.group_hr_user")
-    letter_type_id = fields.Many2one(
-        'ubi.letter.type', string='Бичгийн төрөл', groups="hr.group_hr_user")
-    letter_subject_id = fields.Many2one(
-        'ubi.letter.subject', string='Гарчиг ангилал', groups="hr.group_hr_user")
+    partner_id = fields.Many2one('res.partner', string='Хэнээс', groups="hr.group_hr_user")
+    letter_type_id = fields.Many2one('ubi.letter.type', string='Бичгийн төрөл', groups="hr.group_hr_user")
+    letter_subject_id = fields.Many2one('ubi.letter.subject', string='Гарчиг ангилал', groups="hr.group_hr_user")
 
-    letter_template_id = fields.Many2one(
-        'ubi.letter.template', string='Бичгийн загвар', groups="hr.group_hr_user")
+    letter_template_id = fields.Many2one('ubi.letter.template', string='Бичгийн загвар', groups="hr.group_hr_user")
 
-    letter_template_text = fields.Html(
-        related="letter_template_id.letter_template", groups="hr.group_hr_user")
-    custom_letter_template = fields.Html(
-        'Custom text', groups="hr.group_hr_user")
+    letter_template_text = fields.Html(related="letter_template_id.letter_template", groups="hr.group_hr_user")
+    custom_letter_template = fields.Html('Custom text', groups="hr.group_hr_user")
 
-    department_id = fields.Many2one(
-        'hr.department', string='Хариуцах Хэлтэс', groups="hr.group_hr_user")
-    employee_id = fields.Many2one(
-        'hr.employee', string='Хэнд', groups="hr.group_hr_user")
+    department_id = fields.Many2one('hr.department', string='Хариуцах Хэлтэс', groups="hr.group_hr_user")
+    employee_id = fields.Many2one('hr.employee', string='Хэнд', groups="hr.group_hr_user")
     where_sent = fields.Char(string='Хаашаа явах', groups="hr.group_hr_user")
 
     how_decide = fields.Char(string="Яаж шийдвэрлэх", groups="hr.group_hr_user")
