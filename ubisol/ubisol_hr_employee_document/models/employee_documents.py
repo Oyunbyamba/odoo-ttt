8de47629ef42ae702b8abc1079d48519b83235fd
Ganjidmaa <ganjidmaa@ubisol.mn>
2021-03-31 12:46:52 +0800

    commit

--- ubisol/ubisol_hr_employee_document/models/employee_documents.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from datetime import datetime, date, timedelta
-from odoo import models, fields, api, _
-from odoo.exceptions import Warning
-
-
-class HrEmployeeDocument(models.Model):
-    _inherit = 'hr.employee.document'
