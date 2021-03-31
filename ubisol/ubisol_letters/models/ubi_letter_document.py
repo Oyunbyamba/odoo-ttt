eed2718aad6e9c6f1ff9c9d1e3c2ba628207984d
Ganjidmaa <ganjidmaa@ubisol.mn>
2021-03-31 14:45:58 +0800

    letter import button

--- ubisol/ubisol_letters/models/ubi_letter_document.py
+++ ubisol/ubisol_letters/models/ubi_letter_document.py
@@ -6,5 +6,6 @@ from odoo import fields, models
 class UbiLetterDocument(models.Model):
     _name = "ubi.letter.document"
     _description = " "
-    document_id = fields.Integer( groups="base.group_user")
+
+    document_id = fields.Integer(groups="base.group_user")
     
