5907a3e662204d58385d98d8f136801590489c84
tsenguun-ubisol <80658114+tsenguun-ubisol@users.noreply.github.com>
2021-03-31 14:40:26 +0800

    Revert "letter"
    This reverts commit 57999004475d9e82577f79fb2a6a6cda256f0268.

--- ubisol/ubisol_letters/__manifest__.py
+++ ubisol/ubisol_letters/__manifest__.py
@@ -12,11 +12,8 @@
     'data': [
         'security/ir.model.access.csv',
         'data/data.xml',
-        'views/going.xml',
-        'views/incoming.xml',
-        'views/menu.xml',
-        'views/planning.xml',
-        'views/settings.xml',
+        'views/templates.xml',
+        'views/views.xml',
         'report/letter_detail.xml',
         'report/report.xml',
 
