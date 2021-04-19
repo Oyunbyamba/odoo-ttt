659d1f52eaa14d54f736cc843057b0985d51b156
Sainbayar <sainbayarus@gmail.com>
2021-04-19 17:26:35 +0800

    report papersize

--- ubisol/ubisol_letters/report/letter_detail_a4.py
+++ ubisol/ubisol_letters/report/letter_detail_a4.py
@@ -16,21 +16,32 @@ class letterDetailPdf(models.AbstractModel):
         employee = self.env.user.employee_id
         now_date = (datetime.now()).strftime('%Y-%m-%d')
 
-        paperformat_obj = self.env.ref('ubisol_letters.letter_paperformat').id
-        _logger.info(paperformat_obj)
+        # report = self.env['ir.actions.report']._get_report_from_name(
+        #     'ubisol_letters.letter_detail_report_a5')
+        # report.paperformat_id = self.env.ref(
+        #     'ubisol_letters.letter_paperformat')
 
-        report_obj = self.env['ir.actions.report']
-        report = report_obj._get_report_from_name('ubisol_letters.letter_detail_report_a4')
-        docargs = {
-            'doc_ids': docids,
-            'doc_model': report.model,
+        report = self.env.ref('ubisol_letters.letter_detail_report_a4_pdf')
+        report.paperformat_id = self.env.ref(
+            'ubisol_letters.letter_paperformat').id
+        # paperformat_obj = self.env.ref('ubisol_letters.letter_paperformat').id
+        # _logger.info(paperformat_obj)
+
+        # report_obj = self.env['ir.actions.report']
+        # report = report_obj._get_report_from_name(
+        #     'ubisol_letters.letter_detail_report_a4')
+        # self.paperformat_id = self.env.ref(
+        #    'ubisol_letters.letter_paperformat').id
+
+        data = {
+            'now_date': now_date,
             'docs': docs,
-            'employee': employee
+            'doc_model': report.model,
+            'employee': employee[0]
         }
-        return docargs
+        return report.report_action([], data=data)
 
-    @api.model
-    def get_paperformat(self):
-        # return self.paperformat_id or self.env.company.paperformat_id
-        return self.env.ref('ubisol_letters.letter_paperformat').id
-
+    # @api.model
+    # def get_paperformat(self):
+    #     # return self.paperformat_id or self.env.company.paperformat_id
+    #     return self.env.ref('ubisol_letters.letter_paperformat').id
